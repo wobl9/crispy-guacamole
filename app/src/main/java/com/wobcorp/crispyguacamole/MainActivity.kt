567fcb7b04338b5e84528c3561639186a239518a
woblya <flub.wobl9@gmail.com>
2020-11-03 20:38:49 +0500

    hello compose

--- app/src/main/java/com/wobcorp/crispyguacamole/MainActivity.kt
+++ app/src/main/java/com/wobcorp/crispyguacamole/MainActivity.kt
@@ -2,10 +2,29 @@ package com.wobcorp.crispyguacamole
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            setupHelloWorld()
+        }
+    }
+
+    @Composable
+    fun setupHelloWorld() {
+        Column(
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
+            Text(text = "Dimas")
+            Text(text = "Hello compose!")
+        }
     }
 }
